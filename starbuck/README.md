09b6232d6fcc049d0f20e1b9f736972b69d04d04
BlandShen <1600430050@qq.com>
2020-04-20 21:54:19 +0800

    docs:add four demos

---
+++ starbuck/README.md
@@ -0,0 +1,2 @@
+# starbuck
+Starbucks page
---
+++ 统计数据/echarts.custom.js
@@ -0,0 +1,8 @@
+export * from 'echarts/src/echarts';
+import 'echarts/src/chart/pie';
+import 'echarts/src/chart/line';
+import 'echarts/src/chart/radar';
+import 'echarts/src/component/tooltip';
+import 'echarts/src/component/title';
+import 'echarts/src/component/grid';
+import 'echarts/src/component/legend';
